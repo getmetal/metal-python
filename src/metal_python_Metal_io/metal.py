056b99cf75e60b6ca1362d130396845f87814bae
James O'Dwyer <james@getmetal.io>
2023-02-23 15:21:02 -0800

    init python sdk.

--- src/metal_python_Metal_io/metal.py
+++ src/metal_python_Metal_io/metal.py
@@ -1,3 +1,42 @@
-def add_one(number):
-  return number + 1
+import requests
+
+
+BASE_API = 'https://api.getmetal.io/v1'
+
+
+class Metal:
+  def __init__(self, apiKey, clientId, appId):
+    self.apiKey = apiKey
+    self.clientId = clientId
+    self.appId = appId
   
+  def __getHeaders(self):
+    return {
+      'Content-Type': 'application/json',
+      'x-metal-api-key': self.apiKey,
+      'x-metal-client-id': self.clientId,
+    }
+
+  def index(self, payload = {}):
+    headers = self.__getHeaders()
+    data = { 'app': self.appId, }
+    data.update(payload)
+
+    r = requests.post(BASE_API + '/index', data=data, headers=headers)
+    return r.json()
+  
+  def search(self, payload = {}):
+    headers = self.__getHeaders()
+    data = { 'app': self.appId, }
+    data.update(payload)
+
+    r =  requests.post(BASE_API + '/search', data=data, headers=headers)
+    return r.json()
+  
+  def tune(self, payload = {}):
+    headers = self.__getHeaders()
+    data = { 'app': self.appId, }
+    data.update(payload)
+
+    r =  requests.post(BASE_API + '/tunings', data=data, headers=headers)
+    return r.json()
