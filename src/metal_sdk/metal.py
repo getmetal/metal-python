4db7ae02584c2f3fb670fd15b26721f2f92fffd2
James O'Dwyer <james@getmetal.io>
2023-02-25 08:44:37 -0800

    add more specs.

--- src/metal_sdk/metal.py
+++ src/metal_sdk/metal.py
@@ -5,37 +5,37 @@ BASE_API = 'https://api.getmetal.io/v1'
 
 
 class Metal:
-  def __init__(self, apiKey, clientId, appId):
-    self.apiKey = apiKey
-    self.clientId = clientId
-    self.appId = appId
+  def __init__(self, api_key, client_id, app_id = None):
+    self.api_key = api_key
+    self.client_id = client_id
+    self.app_id = app_id
   
-  def __getHeaders(self):
+  def __get_headers(self):
     return {
       'Content-Type': 'application/json',
-      'x-metal-api-key': self.apiKey,
-      'x-metal-client-id': self.clientId,
+      'x-metal-api-key': self.api_key,
+      'x-metal-client-id': self.client_id,
     }
 
   def index(self, payload = {}):
-    headers = self.__getHeaders()
-    data = { 'app': self.appId, }
+    headers = self.__get_headers()
+    data = { 'app': self.app_id, }
     data.update(payload)
 
     r = requests.post(BASE_API + '/index', data=data, headers=headers)
     return r.json()
   
   def search(self, payload = {}):
-    headers = self.__getHeaders()
-    data = { 'app': self.appId, }
+    headers = self.__get_headers()
+    data = { 'app': self.app_id, }
     data.update(payload)
 
     r =  requests.post(BASE_API + '/search', data=data, headers=headers)
     return r.json()
   
   def tune(self, payload = {}):
-    headers = self.__getHeaders()
-    data = { 'app': self.appId, }
+    headers = self.__get_headers()
+    data = { 'app': self.app_id, }
     data.update(payload)
 
     r =  requests.post(BASE_API + '/tunings', data=data, headers=headers)
--- tests/test_metal.py
+++ tests/test_metal.py
@@ -9,6 +9,12 @@ class TestMetal(unittest.TestCase):
     def test_metal_instantiate(self):
         app_id = 'app-id'
         metal = Metal(API_KEY, CLIENT_ID, app_id)
-        self.assertEqual(metal.apiKey, API_KEY)
-        self.assertEqual(metal.clientId, CLIENT_ID)
-        self.assertEqual(metal.appId, 'app_id')
+        self.assertEqual(metal.api_key, API_KEY)
+        self.assertEqual(metal.client_id, CLIENT_ID)
+        self.assertEqual(metal.app_id, app_id)
+    
+    def test_metal_get_headers(self):
+        metal = Metal(API_KEY, CLIENT_ID)
+        headers = metal._Metal__get_headers()
+        self.assertEqual(headers['x-metal-api-key'], API_KEY)
+        self.assertEqual(headers['x-metal-client-id'], CLIENT_ID)
