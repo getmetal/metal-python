bffc57eb35da37828a97ba91d8029fa2064fcab6
Sergio Prada <chechopradaster@gmail.com>
2023-03-22 22:50:40 -0700

    fix lint and tests

--- examples/example.py
+++ examples/example.py
@@ -8,6 +8,7 @@ metal_api_key = os.environ["METAL_API_KEY"]
 metal_client_id = os.environ["METAL_CLIENT_ID"]
 metal_app_id = os.environ["METAL_APP_ID"]
 
+
 def main():
     metal = Metal(metal_api_key, metal_client_id, metal_app_id)
 
@@ -15,8 +16,8 @@ def main():
     # metal.index({ "id": "666", "text": "Ozzy" })
     # metal.index({ "id": "668", "text": "Bruce Dickenson" })
 
-    result = metal.search({ "text": "Black Sabbath", "limit": 1 })
+    result = metal.search({"text": "Black Sabbath", "limit": 1})
 
-    print(result);
+    print(result)
 
 main()
