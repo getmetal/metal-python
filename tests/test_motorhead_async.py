740f466d3e3da57a7533f7690b95c471885334c8
James O'Dwyer <james@getmetal.io>
2023-05-24 21:29:12 -0400

    update specs

--- tests/test_motorhead_async.py
+++ tests/test_motorhead_async.py
@@ -1,56 +1,37 @@
-import pytest
-import httpx
-from asynctest import CoroutineMock
+from unittest import TestCase, mock
 from src.metal_sdk.motorhead_async import Motorhead
 
 
-@pytest.mark.asyncio
-async def test_initialization():
-    m = Motorhead(api_key='test_key', client_id='test_client')
-    assert m.api_key == 'test_key'
-    assert m.client_id == 'test_client'
-
-    with pytest.raises(ValueError):
-        m = Motorhead()
+API_KEY = "api-key"
+CLIENT_ID = "client-id"
 
 
-@pytest.mark.asyncio
-async def test_add_memory():
-    async with httpx.AsyncClient(transport=httpx.MockTransport(add_memory_mock)) as client:
-        m = Motorhead(api_key='test_key', client_id='test_client')
-        m.client = client
+class TestMotorheadAsync(TestCase):
+    async def test_initialization(self):
+        m = Motorhead(api_key=API_KEY, client_id=CLIENT_ID)
+        self.assertEqual(m.api_key, API_KEY)
+        self.assertEqual(m.client_id, CLIENT_ID)
+
+        with self.assertRaises(ValueError):
+            Motorhead()
+
+    async def test_add_memory(self):
+        m = Motorhead(api_key=API_KEY, client_id=CLIENT_ID)
+        m.client.post = mock.MagicMock(return_value=mock.Mock(status_code=201))
 
         memory = await m.add_memory('test_session', {'key': 'value'})
-        assert memory == 'mock_memory'
+        self.assertEqual(memory, 'mock_memory')
 
-
-@pytest.mark.asyncio
-async def test_get_memory():
-    async with httpx.AsyncClient(transport=httpx.MockTransport(get_memory_mock)) as client:
-        m = Motorhead(api_key='test_key', client_id='test_client')
-        m.client = client
+    async def test_get_memory(self):
+        m = Motorhead(api_key=API_KEY, client_id=CLIENT_ID)
+        m.client.get = mock.MagicMock(return_value=mock.Mock(status_code=200))
 
         memory = await m.get_memory('test_session')
-        assert memory == 'mock_memory'
+        self.assertEqual(memory, 'mock_memory')
 
-
-@pytest.mark.asyncio
-async def test_delete_memory():
-    async with httpx.AsyncClient(transport=httpx.MockTransport(delete_memory_mock)) as client:
-        m = Motorhead(api_key='test_key', client_id='test_client')
-        m.client = client
+    async def test_delete_memory(self):
+        m = Motorhead(api_key=API_KEY, client_id=CLIENT_ID)
+        m.client.delete = mock.MagicMock(return_value=mock.Mock(status_code=204))
 
         memory = await m.delete_memory('test_session')
-        assert memory == 'mock_memory'
-
-
-async def add_memory_mock(request):
-    return httpx.Response(200, json={'data': 'mock_memory'})
-
-
-async def get_memory_mock(request):
-    return httpx.Response(200, json={'data': 'mock_memory'})
-
-
-async def delete_memory_mock(request):
-    return httpx.Response(200, json={'data': 'mock_memory'})
+        self.assertEqual(memory, 'mock_memory')
