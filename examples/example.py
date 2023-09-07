61f3778df9baa5a5931811e95e2e69d68df64ed4
Pablo Rios <pariosur@gmail.com>
2023-09-07 14:48:26 +0100

    fixing lint

--- examples/example.py
+++ examples/example.py
@@ -10,13 +10,9 @@ metal_index_id = os.environ["METAL_INDEX_ID"]
 
 
 def main():
-
     metal = Metal(metal_api_key, metal_client_id, metal_index_id)
 
     # metal.index({ "id": "667", "text": "Dave Mustain" })
     # metal.index({ "id": "666", "text": "Ozzy" })
     # metal.index({ "id": "668", "text": "Bruce Dickenson" })
-    # result = metal.search({"text": "908", "limit": 1})
-    # print(result)
-
-main()
+    metal.search({"text": "908", "limit": 1})
